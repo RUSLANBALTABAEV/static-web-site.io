6304eb725c854ee0398f3e3a712d0ed49e045f21
RUSLAN BALTABAEV <ruslanotoriuos@gmail.com>
2023-12-11 11:59:52 +0600

    Added animation slide

--- script.js
+++ script.js
@@ -16,3 +16,25 @@ function showService(serviceId) {
     const selectedService = document.getElementById(serviceId);
     selectedService.classList.add('active');
 }
+
+let currentAchievementIndex = 0;
+const achievementsList = document.getElementById('achievements-list').querySelectorAll('li');
+
+function showAchievement(index) {
+    const translateValue = -index * 100 + '%';
+    document.getElementById('achievements-list').style.transform = 'translateX(' + translateValue + ')';
+    currentAchievementIndex = index;
+}
+
+function nextAchievement() {
+    currentAchievementIndex = (currentAchievementIndex + 1) % achievementsList.length;
+    showAchievement(currentAchievementIndex);
+}
+
+function prevAchievement() {
+    currentAchievementIndex = (currentAchievementIndex - 1 + achievementsList.length) % achievementsList.length;
+    showAchievement(currentAchievementIndex);
+}
+
+// Автоматическая смена достижений
+setInterval(nextAchievement, 5000); // Менять каждые 5 секунд
